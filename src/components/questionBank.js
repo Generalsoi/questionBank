3e7f515d4d82e11f7803f1e8278672b6bb61b132
Success <ikhinobelesuccess@gmail.com>
2022-10-10 00:59:19 +0100

    updates

---
+++ src/components/questionBank.js
@@ -0,0 +1,32 @@
+const questionBank = {
+  1: {
+    question: "What is the definition of computer?",
+    options: {
+      a: "Computer is an electronic device used for transferring data",
+      b: "Computer is a toy",
+      c: "Computer is a dangerous weapon",
+      d: "Computer is a machine",
+    },
+    answer: "a",
+  },
+  2: {
+    question: "What is the definition of computer?",
+    options: {
+      a: "Computer is an electronic device used for transferring data",
+      b: "Computer is a toy",
+      c: "Computer is a dangerous weapon",
+      d: "Computer is a machine",
+    },
+    answer: "a",
+  },
+  3: {
+    question: "What is the definition of computer?",
+    options: {
+      a: "Computer is an electronic device used for transferring data",
+      b: "Computer is a toy",
+      c: "Computer is a dangerous weapon",
+      d: "Computer is a machine",
+    },
+    answer: "a",
+  },
+};
