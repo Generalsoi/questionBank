40bc3cbc7e6d6810cdeb64539e6a70ced8a88158
Success <ikhinobelesuccess@gmail.com>
2022-10-10 01:08:27 +0100

    updates

--- src/components/questions.jsx
+++ src/components/questions.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 export const Questions = () => {
-  const [question, setQuestion] = useState("");
+  const [question, setQuestion] = useState("0");
   const [display, setDisplay] = useState(false);
 
   const questionBank = {
@@ -41,8 +41,8 @@ export const Questions = () => {
   console.log(questionBankJson);
 
   return (
-    <div className="w-full h-screen flex items-center font-roboto bg-black text-white">
-      <div className="h-screen w-1/5 flex items-center justify-center">
+    <div className="w-full h-screen flex flex-col md:flex-row  items-center font-roboto bg-black text-white">
+      <div className="h-screen w-full md:w-1/5 flex items-center justify-center">
         <div className="flex flex-col items-center justify-center gap-4 w-full border border-white h-[30%] p-3">
           <input
             type="search"
@@ -59,9 +59,9 @@ export const Questions = () => {
           </button>
         </div>
       </div>
-      <div className="h-screen w-4/5 flex items-center justify-center">
+      <div className="h-screen w-full md:w-4/5 flex items-center justify-center">
         <div className="w-[80%] h-[80%] flex flex-col items-center justify-center border border-white rounded-lg">
-          {display && questionBank[question].question}
+          {!!display && questionBank[question].question}
           <button>Show answer</button>
         </div>
       </div>
